2c53a2b6dd24b14459ba879e6dd55f80fb875e09
ElronS207 <85743154+ElronS207@users.noreply.github.com>
2021-08-13 11:41:13 +0530

    Add files via upload

---
+++ Project_Template_24-main/js/playerArcher.js
@@ -0,0 +1,35 @@
+class playerArcher{
+    constructor(x,y,width,height){
+        var options = {
+            isStatic: true
+        };
+
+        this.body = Matter.Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("./assets/playerArcher.png");
+        World.add(world,this.body)
+        Matter.Body.setAngle(this.body, -PI / 2);
+    }
+
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);
+        
+        pop()
+
+    }
+
+    shoot(archerAngle){
+        var velocity = p5.Vector.fromAngle(archerAngle);
+        velocity.mult(20);
+        Matter.Body.setStatic(this.body,false);
+        Matter.Body.setVelocity(this.body,{x: velocity.x, y: velocity.y});
+    
+    }
+}
